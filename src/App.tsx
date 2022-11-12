9ed31f10f4589327dda7bea1c9ce2540f3d76d66
Fell1p <luofelipe@hotmail.com>
2022-11-12 20:38:09 -0300

    Criação do primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Botao from './components/Botao'; //importação do botão criado
 
-function App() {
-  return (
+function App() { //function que reindenizara nosso componente
+  return ( //onde será criada a div responsavél por exibir nosso componente
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Botao/>
     </div>
   );
 }
---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; //importação do react em nosso codigo
+
+class Botao extends React.Component { //criação de um componente
+    render(): React.ReactNode {
+        return (
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
+
+export default Botao; //Exportação do nosso componente
