39be8b0dac00a681f68e1180414f0be69988e494
Fell1p <luofelipe@hotmail.com>
2022-11-13 22:15:00 -0300

    Criação do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Botao from './components/Botao'; //importação do botão criado
+import Formulario from './components/Formulario'; //importação do botão criado
 
 function App() { //function que reindenizara nosso componente
   return ( //onde será criada a div responsavél por exibir nosso componente
     <div className="App">
-      <Botao/>
+      <Formulario />
     </div>
   );
 }
--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'; //importação do react em nosso codigo
 
-class Botao extends React.Component { //criação de um componente
+class Botao extends React.Component { //criação de um componente e o nome do componente deve sempre ser escrito com a primeira letra maiuscula
     render(): React.ReactNode {
         return (
             <button>
