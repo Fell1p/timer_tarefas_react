92ed3b82fc82d2c786c724bc41a1f2c776380739
Fell1p <luofelipe@hotmail.com>
2022-11-13 23:13:36 -0300

    Utilização de uma array de objetos para reindenização dinamica do componente Lista

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -3,26 +3,31 @@ import React from "react";
 //Utilização da function component, neste processo utiliza-se a sintaxe do JS para a criação dos componentes.
 
 function Lista() {
-    return (
+    const tarefas = [{  //Criação de uma array de ojetos onde adicionaremos li's de maneira dinamica, assim, diminuindo linhas de codigo
+        tarefa: 'React',
+        tempo: '02.00:00'
+    }, 
+    {
+        tarefa: 'JavaScript',
+        tempo: '01:00:00'
+    },
+    {
+        tarefa: "typeScript",
+        tempo: '03:00:00'
+    }]
+    return ( 
+            /*-Reindenização do codigo, onde se utiliza a tag aside para separação de uma seção para o conteudo que sera retornado
+            - A simples execução retornará apenas uma arrazy, porém queremos que a array seja percorria e que um html seja retornado e para isso utilizamos o map
+            - map: O map é uma função JavaScript que permitira a array ser percorrida e retornar um html, o mesmo aceita 2 parametros como */
         <aside>
             <h2> Estudos do dia</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        JavaScript  
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tarefas.map((item, index) => (
+                    <li key={index}>
+                        <h3> {item.tarefa} </h3>
+                        <span> {item.tempo} </span>
+                    </li>
+                ))}                
             </ul>
         </aside>
     )
