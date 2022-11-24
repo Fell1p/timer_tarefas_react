96f6f3c08192355c3a4f7b647e7ab5dabd24e584
Fell1p <luofelipe@hotmail.com>
2022-11-23 22:20:23 -0300

    Interação entre relogio e tarefa

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,7 +2,7 @@ import Botao from '../Botao';
 import Relogio from './Relogio'
 import style from './Cronometro.module.scss'
 import { Itarefa } from '../../types/tarefa';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { tempoParaSegundos } from '../../common/utils/time';
 
 interface Props {
@@ -11,15 +11,17 @@ interface Props {
 
 export default function Cronometro({selecionado}: Props) {
     const [tempo, setTempo] = useState<number>();
-    if(selecionado?.tempo){
-        setTempo(tempoParaSegundos(selecionado.tempo))
-    }
+
+    useEffect(() => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado])
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-            tempo: {tempo}
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo} />
             </div>
             <Botao>
                 Começar!
